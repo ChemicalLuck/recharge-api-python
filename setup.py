34d612b8c0f0e53fcb2b87933ced91f8102da7e8
ChemicalLuck <J.Tebbett@outlook.com>
2021-07-23 10:00:17 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setup(
     name='recharge-api',
-    version='1.3.0',
+    version='1.4.0',
     author='ChemicalLuck',
     author_email='chemicalluck@outlook.com',
     packages=['recharge'],
